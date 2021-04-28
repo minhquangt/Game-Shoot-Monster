56efdc3fec9e5f995865352a0e87f1a04027ea32
- <->
2021-04-28 08:27:07 +0700

    game SDL_2

---
+++ Desktop/GAME_SDL_2/GAME_SDL_2/BaseObject.cpp
@@ -0,0 +1,53 @@
+﻿#include"BaseObject.h"
+
+BaseObject::BaseObject()
+{
+	p_object_ = NULL;
+	rect_.x = 0;
+	rect_.y = 0;
+	rect_.w = 0;
+	rect_.h = 0;
+}
+
+BaseObject::~BaseObject()
+{
+	Free();
+}
+bool BaseObject::LoadImg(std::string path, SDL_Renderer* screen)
+{
+	Free();
+
+	SDL_Texture* new_texture = NULL;
+	SDL_Surface* load_suface = IMG_Load(path.c_str());
+	if (load_suface != NULL)
+	{
+		SDL_SetColorKey(load_suface, SDL_TRUE, SDL_MapRGB(load_suface->format, COLOR_KEY_R, COLOR_KEY_G, COLOR_KEY_B)); //xóa background của nhân vật game
+		new_texture = SDL_CreateTextureFromSurface(screen, load_suface);
+		if (new_texture != NULL)
+		{
+			rect_.w = load_suface->w;
+			rect_.h = load_suface->h;
+
+		}
+		SDL_FreeSurface(load_suface);
+	}
+	p_object_ = new_texture;
+	return p_object_ != NULL;
+}
+
+void BaseObject::Render(SDL_Renderer* des, const SDL_Rect* clip)
+{
+	SDL_Rect renderquad = { rect_.x, rect_.y, rect_.w, rect_.h };
+	SDL_RenderCopy(des, p_object_, clip, &renderquad);
+}
+
+void BaseObject::Free()
+{
+	if (p_object_ != NULL)
+	{
+		SDL_DestroyTexture(p_object_);
+		p_object_ = NULL;
+		rect_.w = 0;
+		rect_.h = 0;
+	}
+}
---
+++ Desktop/GAME_SDL_2/GAME_SDL_2/BaseObject.h
@@ -0,0 +1,24 @@
+﻿#ifndef BASE_OBJECT_H_
+#define BASE_OBJECT_H_
+
+#include"commonfunc.h"
+class BaseObject
+{
+public:
+	BaseObject();
+	~BaseObject();
+
+	void SetRect(const int& x, const int& y) { rect_.x = x, rect_.y = y; }
+	SDL_Rect GetRect() const { return rect_; }
+	SDL_Texture* GetObject() const { return p_object_; }
+
+	virtual bool LoadImg(std::string path, SDL_Renderer* screen); //Load ảnh lên đối tượng p_object_, sử dụng p_object_ để thêm nó vào màn hình
+	void Render(SDL_Renderer* des, const SDL_Rect* clip = NULL);
+	void Free();
+protected:
+	SDL_Texture* p_object_; // biến lưu trữ hình ảnh
+	SDL_Rect rect_; // lưu kích thước của ảnh
+};
+#endif;
+
+
